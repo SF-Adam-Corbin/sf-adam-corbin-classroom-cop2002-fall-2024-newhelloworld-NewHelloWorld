ae50c3167da08740ce293bfb7f74c0bfc454abd5
Trey Smith <treysokmith@gmail.com>
2024-08-28 23:28:34 -0400

    mine.py

--- mine.py
+++ mine.py
@@ -1,5 +1,5 @@
 # Write a line of code that prints "Hello world!".
-print(Hello_world!().)
+print("Hello world!")
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
